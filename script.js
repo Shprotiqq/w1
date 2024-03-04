26a33a0423934a820062f632b1544d6162043715
Dmitry Shapar <blackdragon01@yandex.ru>
2024-03-04 20:43:48 +0500

    Added scripts for step progress bar

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const link = document.querySelector('.sublist__button')
 const subList = document.querySelector('.navigation__sublist')
 const arrow = document.querySelector('.button-link__arrow_1')
+const progress = document.getElementById("progress")
+const prev = document.querySelector(".button-prev")
+const next = document.querySelector(".button-next")
+const circles = document.querySelectorAll(".progress_circle")
+let currentActive = 1
+
 
 function sublistShow (event) {
     if (subList.classList.contains("hidden") === true) {
@@ -32,3 +38,34 @@ link.addEventListener("click", reverseArrow);
 link.addEventListener("click", reverseArrowReset)
 
 // НЕ РАБОТАЕТ
+
+next.addEventListener("click", () => {
+    currentActive++;
+    if (currentActive > circles.length) currentActive = circles.length;
+    update();
+});
+
+prev.addEventListener("click", () => {
+    currentActive--;
+    if (currentActive < 1) currentActive = 1;
+    update();
+});
+
+const update = () => {
+    circles.forEach((circle, index) => {
+        if (index < currentActive) circle.classList.add("active");
+        else circle.classList.remove("active");
+    });
+    const actives = document.querySelectorAll(".active");
+    progress.style.width =
+        ((actives.length - 1) / (circles.length - 1)) * 100 + "%";
+    if (currentActive === 1) prev.disabled = true;
+    else if (currentActive === circles.length) next.disabled = true;
+    else {
+        prev.disabled = false;
+        next.disabled = false;
+    }
+};
+
+
+
