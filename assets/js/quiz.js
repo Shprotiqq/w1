59f1608b423cb1038c29ef7dde6991ef842a83a9
Dmitry Shapar <blackdragon01@yandex.ru>
2024-03-20 04:59:00 +0500

    Finalized quiz

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,25 +1,26 @@
-const progressBar = document.getElementById("progress")
-let prev = document.querySelector(".button-prev")
-let next = document.querySelector(".button-next")
-const circles = document.querySelectorAll(".progress_circle")
-const choose = document.querySelectorAll(".variants_item")
-let currentActive = 1
+const progressBar = document.getElementById("progress");
+const variantBtn = document.querySelectorAll('.input_quiz');
+const variants = []
+let prev = document.querySelector(".button-prev");
+let next = document.querySelector(".button-next");
+const circles = document.querySelectorAll(".progress_circle");
+const slides = $('.quiz_variants');
+var currentStep = slides[0];
+var currentSlide = 0;
+let currentActive = 1;
 
-next.addEventListener("click", () => {
-    currentActive++;
-    if (currentActive > circles.length) {
-        currentActive = circles.length;
-    }
-    update();
-});
-
-prev.addEventListener("click", () => {
-    currentActive--;
-    if (currentActive < 1) {
-        currentActive = 1;
-    }
-    update();
-});
+variantBtn.forEach(el => {
+    el.addEventListener('click', e => {
+        let self = e.currentTarget;
+        let variant = self.value;
+        let parent = self.closest('.variants_item');
+        let currenntSlide = parent.closest('.quiz_variants ')
+        parent.classList.add('input_quiz-active')
+        currenntSlide.classList.add('input_quiz-disabled')
+        variants.push(variant)
+        localStorage.setItem('quiz_answers', variants)
+    })
+})
 
 const update = () => {
     let width = 0;
@@ -43,17 +44,39 @@ const update = () => {
     }
 };
 prev.addEventListener("click", () => {
+    currentActive--;
+    if (currentActive < 1) {
+        currentActive = 1;
+    }
+    update();
+
     const number = document.querySelector("#number");
     const result = Number(number.innerHTML) - 1;
-
+    currentSlide--;
+    let prevStep = slides[currentSlide];
+    currentStep.classList.remove('quiz_variants-active');
+    prevStep.classList.add('quiz_variants-active');
+    currentStep = prevStep;
     if (result > 0) {
         number.innerHTML = result;
     }
 });
 
 next.addEventListener("click", () => {
-    const number = document.querySelector("#number");
+    currentActive++;
+    if (currentActive > circles.length) {
+        currentActive = circles.length;
+    }
+    update();
 
+    const number = document.querySelector("#number");
     number.innerHTML = Number(number.innerHTML) + 1;
+    currentSlide++;
+    let nextStep = slides[currentSlide];
+    currentStep.classList.remove('quiz_variants-active');
+    nextStep.classList.add('quiz_variants-active');
+    currentStep = nextStep;
 });
+
+
 
