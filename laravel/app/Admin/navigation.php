33e63b7e1add04a7716f6ba073ef75a34dad2fdc
Goblinchic <maximf98@mail.ru>
2024-04-07 22:36:49 +0500

    Add admin panel

---
+++ laravel/app/Admin/navigation.php
@@ -0,0 +1,86 @@
+<?php
+
+use App\Models\Brand;
+use SleepingOwl\Admin\Navigation\Page;
+
+// Default check access logic
+// AdminNavigation::setAccessLogic(function(Page $page) {
+// 	   return auth()->user()->isSuperAdmin();
+// });
+//
+// AdminNavigation::addPage(\App\User::class)->setTitle('test')->setPages(function(Page $page) {
+// 	  $page
+//		  ->addPage()
+//	  	  ->setTitle('Dashboard')
+//		  ->setUrl(route('admin.dashboard'))
+//		  ->setPriority(100);
+//
+//	  $page->addPage(\App\User::class);
+// });
+//
+// // or
+//
+// AdminSection::addMenuPage(\App\User::class)
+
+return [
+    [
+        'title' => 'Dashboard',
+        'icon'  => 'fas fa-tachometer-alt',
+        'url'   => route('admin.dashboard'),
+    ],
+
+    [
+        'title' => 'Information',
+        'icon'  => 'fas fa-info-circle',
+        'url'   => route('admin.information'),
+    ],
+
+     [
+        'title' => 'Брендирование',
+        'pages' => [
+
+//            Brand::class,
+
+            // or
+
+            (new Page(Brand::class))
+                ->setPriority(100)
+                ->setIcon('fas fa-users')
+                ->setUrl('brands'),
+
+            // or
+
+//            new Page([
+//                'title'    => 'News',
+//                'priority' => 200,
+//                'model'    => \App\News::class
+//            ]),
+//
+//            // or
+//            (new Page(/* ... */))->setPages(function (Page $page) {
+//                $page->addPage([
+//                    'title'    => 'Blog',
+//                    'priority' => 100,
+//                    'model'    => \App\Blog::class
+//			      ));
+//
+//			      $page->addPage(\App\Blog::class);
+//    	      }),
+//
+//            // or
+//
+//            [
+//                'title'       => 'News',
+//                'priority'    => 300,
+//                'accessLogic' => function ($page) {
+//                    return $page->isActive();
+//    		      },
+//                'pages'       => [
+//
+//                    // ...
+//
+//                ]
+//            ]
+        ]
+     ]
+];
---
+++ laravel/app/Providers/AdminSectionsServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use SleepingOwl\Admin\Providers\AdminSectionsServiceProvider as ServiceProvider;
+
+class AdminSectionsServiceProvider extends ServiceProvider
+{
+
+    /**
+     * @var array
+     */
+    protected $sections = [
+        \App\Models\User::class => 'App\Http\Admin\Users',
+        \App\Models\Brand::class => 'App\Http\Admin\Brands',
+    ];
+
+    /**
+     * Register sections.
+     *
+     * @param \SleepingOwl\Admin\Admin $admin
+     * @return void
+     */
+    public function boot(\SleepingOwl\Admin\Admin $admin)
+    {
+    	//
+
+        parent::boot($admin);
+    }
+}
